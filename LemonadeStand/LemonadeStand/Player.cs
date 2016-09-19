7c9fe87af2b40a0df4a2c93b73cd93475e111229
Benjamin <bsterner6696@gmail.com>
2016-09-19 09:49:23 -0500

    initial classes made, begin stubbing out code

---
+++ LemonadeStand/LemonadeStand/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    abstract public class Player
+    {
+        Inventory inventory;
+        Double money;
+        string name;
+         
+    }
+}
